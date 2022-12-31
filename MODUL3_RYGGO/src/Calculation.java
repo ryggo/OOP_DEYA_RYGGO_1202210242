ff4075440e6612e7c9470747ce15788a5c414cbb
ryggo <52133499+ryggo@users.noreply.github.com>
2022-12-31 07:41:59 +0700

    MODUL3_RYGGO

---
+++ MODUL3_RYGGO/src/Calculation.java
@@ -0,0 +1,68 @@
+public class Calculation implements Runnable {
+    public double radius;
+    public double side;
+    public double area;
+    public double phi = 3.14;
+    
+    public void setSquare(double side) {
+        this.side = side;
+            try {
+                if (side < 1){
+                    throw new IllegalArgumentException("panjang sisi tidak boleh kurang dari 1");
+                } else {
+                    area = side * side;
+                }
+            } catch (NumberFormatException e) {
+                e.getMessage();
+            }
+    }
+    
+    public double getSquare() {
+        return area;
+    }
+    
+    public void setCircle( double radius) {
+        this.radius = radius;
+            try {
+                if (radius < 1){
+                       throw new IllegalArgumentException("panjang radius tidak boleh kurang dari 1");
+                } else {
+                    area = phi * radius;
+                }
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+    }
+
+    public double getCircle(){
+        return area;
+    }
+    
+    public void setTrapezoid(double a, double b, double t) {
+            try {
+                if (a < 1|| b < 1|| t < 1){
+                    throw new IllegalArgumentException("panjang sisi tidak boleh kurang dari 1");
+                } else {
+                    area = ((a + b) * t)/2;
+                }
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+    }
+    public double getTrapezoid(){
+        return area;
+    }
+    
+    @Override
+    public void run(){
+        System.out.println("===== Program will start in =====");
+        for (int i = 3; i > 0; i --)
+            try {
+                System.out.println("Starting with thread "+i);
+                Thread.sleep(1500);
+            } catch (InterruptedException e){
+                e.printStackTrace();
+            }
+    } 
+}
+
---
+++ MODUL3_RYGGO/src/Main.java
@@ -0,0 +1,58 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        boolean repeat = true;
+        Scanner input = new Scanner(System.in);
+        Calculation c = new Calculation();
+        int pilihmenu;
+
+        do {
+            System.out.println("==Menu Program==\n1. Square\n2. Circle\n3. Trapezoid\n0. Finish");
+            try {
+                System.out.println("Select menu : ");
+                pilihmenu = input.nextInt();
+                switch(pilihmenu){
+                    case 1:
+                        System.out.println("Enter the length of side of the square : ");
+                        double side = input.nextDouble();
+                        c.setSquare(side);
+                        c.run();
+                        System.out.println("the calculation result : " +c.getSquare());
+                        break;
+                    case 2:
+                        System.out.println("Enter the radius of the circle : ");
+                        double radius = input.nextDouble();
+                        c.setCircle(radius);
+                        c.run();
+                        System.out.println("the calculation result : " +c.getCircle());
+                        break;
+                    case 3:
+                        System.out.println("insert the side of the base of the trapezoid : ");
+                        double p = input.nextDouble();
+                        System.out.println("enter the upper side of the trapezoid : ");
+                        double l = input.nextDouble();
+                        System.out.println("enter the height of the trapezoid : ");
+                        double t = input.nextDouble();
+                        c.setTrapezoid(p,l,t);
+                        c.run();
+                        System.out.println("the calculation result : " +c.getTrapezoid());
+                        break;
+                    case 0:
+                        System.out.println("program has ended.");
+                        break;
+                    default:
+                        System.out.println("option not available");
+                        continue;
+                }
+                repeat = false;
+            } catch (InputMismatchException e){
+                System.out.println("==== ERROR : INPUT MUST BE A NUMBER ====");
+                input.next();
+            } catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+        } while (repeat);
+        input.close();
+    }
+}
